d3d617247f4d7785955d80016ca15368ae25b044
=Elvireak <=e.akayezu@alustudent.com>
2026-09-18 22:26:51 +0400

    Added the the oth file and README.md

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fixes a 500 error in WordPress by correcting a .phpp typo in wp-settings.php
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin'],
+}
